5b13f56f37020298fe2d7e0192798c0d367b6081
john morris <fennymorris@live.co.uk>
2020-03-28 21:52:15 +0000

    use render expression

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -8,8 +8,28 @@ export default class ListAppointments extends Component {
       )
     })
     return (
-      <div>
-        {listItems}
+      <div className="appointment-list item-list mb-3">
+        {this.props.myAppointments.map(item => (
+          <div className="pet-item col media py-3">
+            <div className="mr-3">
+              <button className="pet-delete btn btn-sm btn-danger">X</button>
+            </div>
+
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span className="pet-name">{item.petName}</span>
+                <span className="apt-date ml-auto">{item.aptDate}</span>
+              </div>
+
+              <div className="owner-name">
+                <span className="label-item">Owner: </span>
+                <span>{item.ownerName}</span>
+              </div>
+              <div className="apt-notes">{item.aptNotes}</div>
+            </div>
+          </div>
+        ))}
+
       </div>
     )
   }
