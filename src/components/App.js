e295378d76bedcd60f6286981afbf792c1cac63a
john morris <fennymorris@live.co.uk>
2020-03-28 16:51:58 +0000

    change apptype to class based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
+// import React, {useState} from 'react';
+// import '../css/App.css';
+// import AddAppointments from './AddAppointments';
+// import SearchAppointments from './SearchAppointments';
+// import ListAppointments from './ListAppointments';
+
+// function App() {
+//   const [myName, setMyName] = useState('Rasdfy')
+//   return (
+//     <main className="page bg-white" id="petratings">
+//       <div className="container">
+//         <div className="row">
+//           <div className="col-md-12 bg-white">
+//             <div className="container">
+//               {myName}
+//               <div><AddAppointments /></div>
+//               <div><SearchAppointments/></div>
+//               <div><ListAppointments/></div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+// class type
 import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <div><AddAppointments /></div>
-              <div><SearchAppointments/></div>
-              <div><ListAppointments/></div>
+export default class App extends Component {
+  state = {
+    myName: 'JohnMorris'
+  }
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.myName}
+                <div><AddAppointments /></div>
+                <div><SearchAppointments /></div>
+                <div><ListAppointments /></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    )
+  }
 }
-
-export default App;
