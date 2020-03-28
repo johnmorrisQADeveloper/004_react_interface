6e65ea7cc07cf8ccc625490c6e3454e259880af8
john morris <fennymorris@live.co.uk>
2020-03-28 11:33:21 +0000

    add modules to index.js
    by doing so its available in the whole project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "bootstrap/dist/css/bootstrap.css"
+import "jquery/dist/jquery.js"
+import "popper.js/dist/umd/popper.js"
+import "bootstrap/dist/js/bootstrap.js"
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
