1f219e50207744adf6920baabccacea3d19a1361
john morris <fennymorris@live.co.uk>
2020-03-28 22:24:14 +0000

    change app to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,87 @@
-// import React, {useState} from 'react';
-// import '../css/App.css';
-// import AddAppointments from './AddAppointments';
-// import SearchAppointments from './SearchAppointments';
-// import ListAppointments from './ListAppointments';
-
-// function App() {
-//   const [myName, setMyName] = useState('Rasdfy')
-//   return (
-//     <main className="page bg-white" id="petratings">
-//       <div className="container">
-//         <div className="row">
-//           <div className="col-md-12 bg-white">
-//             <div className="container">
-//               {myName}
-//               <div><AddAppointments /></div>
-//               <div><SearchAppointments/></div>
-//               <div><ListAppointments/></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
-// export default App;
-
-
-// class type
+import React, { useState, useEffect } from 'react';
 import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
-import React, { Component } from 'react'
-export default class App extends Component {
-  state = {
-    myName: 'JohnMorris',
-    myAppointments: [],
-    lastIndex: 0
-  }
-  componentDidMount() {
+
+function App() {
+  const [myAppointments, setmyAppointments] = useState([])
+  let [lastIndex, setLastIndex] = useState(0)
+
+  useEffect(() => {
     fetch('./data.json')
       .then(response => response.json())
       .then(result => {
         const apts = result.map(item => {
-          item.aptId = this.state.lastIndex
-          this.setState({ lastIndex: this.state.lastIndex + 1 })
+          item.aptId = lastIndex
+          setLastIndex(lastIndex)
+          lastIndex ++
           return item
         })
-        this.setState({
-          myAppointments: apts
-        })
+        setmyAppointments(apts)
       })
-  }
-  render() {
-    return (
-      <main className="page bg-white" id="petratings">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 bg-white">
-              <div className="container">
-                <AddAppointments />
-                <SearchAppointments />
-                <ListAppointments myAppointments={this.state.myAppointments} />
-              </div>
+  }, [lastIndex])
+  return (
+    <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+              <div><AddAppointments /></div>
+              <div><SearchAppointments /></div>
+              <div><ListAppointments myAppointments={myAppointments}/></div>
             </div>
           </div>
         </div>
-      </main>
-    )
-  }
+      </div>
+    </main>
+  );
 }
+
+export default App;
+
+
+// class type
+// import '../css/App.css';
+// import AddAppointments from './AddAppointments';
+// import SearchAppointments from './SearchAppointments';
+// import ListAppointments from './ListAppointments';
+// import React, { Component } from 'react'
+// export default class App extends Component {
+//   state = {
+//     myName: 'JohnMorris',
+//     myAppointments: [],
+//     lastIndex: 0
+//   }
+//   componentDidMount() {
+//     fetch('./data.json')
+//       .then(response => response.json())
+//       .then(result => {
+//         const apts = result.map(item => {
+//           item.aptId = this.state.lastIndex
+//           this.setState({ lastIndex: this.state.lastIndex + 1 })
+//           return item
+//         })
+//         this.setState({
+//           myAppointments: apts
+//         })
+//       })
+//   }
+//   render() {
+//     return (
+//       <main className="page bg-white" id="petratings">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-md-12 bg-white">
+//               <div className="container">
+//                 <AddAppointments />
+//                 <SearchAppointments />
+//                 <ListAppointments myAppointments={this.state.myAppointments} />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </main>
+//     )
+//   }
+// }
